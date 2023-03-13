887c5f8cfe77d68366e738959d29c2ecd6439209
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-03-13 14:17:17 +0100

    chore: remove console.log

--- contracts/DecisionNFT.sol
+++ contracts/DecisionNFT.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
 import "./DisputeInitiation.sol";
 
 contract MkpebiResolutio is ERC1155, Ownable {
