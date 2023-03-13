12eb304bd8469692f34e4e8933f8ead455817d47
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-03-13 14:22:17 +0100

    chore: fix deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,31 @@
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  const whitelister = await hre.ethers.getContractFactory("ArbiterWhitelister");
-  const lock = await whitelister.deploy();
+  //Create an instance of the arbiter smart contract
+  const arbiter = await ethers.getContractFactory("ArbiterWhitelister");
+  const arbiterContract = await arbiter.deploy();
+  await arbiterContract.deployed();
+  console.log("arbiterContract deployed to:", arbiterContract.address);
 
-  await lock.deployed();
+  const disputePool = await hre.ethers.getContractFactory("Disputepool");
+  const disputePoolContract = await disputePool.deploy();
+  await disputePoolContract.deployed();
+  console.log("Dispute pool deployed to:", disputePoolContract.address);
 
-  console.log(
-    `deployed to ${lock.address}`
-  );
+  // const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/{INFURA_API}')
+  // const signer = provider.getSigner(SOME_RANDOM_ETH_ADDRESS)
+  // const instance = new ethers.Contract(contractAddress, abi, signer)
+  // const tx = await instance.transfer(MALICIOUS_ADDRESS, 10000000000000000)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
