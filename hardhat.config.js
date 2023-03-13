feb3437a0de7ec8fcff583333679992a87dc113c
Alexandra ogubuike <ogubuikealex@gmail.com>
2023-03-13 14:15:41 +0100

    chore: update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `${process.env.API_URL}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+   etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: `${process.env.YOUR_ETHERSCAN_API_KEY}`
+  },
+  paths: {
+    artifacts: "./src/artifacts",
+  },
 };
