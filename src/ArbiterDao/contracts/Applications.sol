1cef2c1ee8e7e6f9734ba704b7ffe991ee593b30
king-Alex-d-great <ogubuikealex@gmail.com>
2023-12-18 20:41:18 +0100

    Chore: Add Temp Contract Code For Arbiter DAO

---
+++ src/ArbiterDao/contracts/Applications.sol
@@ -0,0 +1,25 @@
+// // contracts/Box.sol
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
+
+// import "@openzeppelin/contracts/access/Ownable.sol";
+
+// contract Application is Ownable {
+//   uint256 public appicationID;
+//   address public applicant;
+//   string public applicationData;
+//   bool public isAccepted;
+
+
+
+//   // Stores a new value in the contract
+//   function store(uint256 newValue) public onlyOwner {
+//     value = newValue;
+//     emit ValueChanged(newValue);
+//   }
+
+//   // Reads the last stored value
+//   function retrieve() public view returns (uint256) {
+//     return { 'applicationID' : appicationID, 'applicantAddress': applicant, 'applicationData' : applicationData, 'isAccepted': isAccepted}
+//   }
+// }
